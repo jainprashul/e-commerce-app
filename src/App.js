ed009ffb969894f76f915b79f72577b292e3c095
Prashul Jain <jainprashul@gmail.com>
2019-08-30 17:02:38 +0530

    React Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Navbar from './components/Navbar';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/PageNotFound/Default";
 
 function App() {
   return (
-    
+    <Router>
       <React.Fragment>
-        <Navbar/>
+        <Navbar />
+
+        <Switch>
+          <Route exact path='/' component={ProductList}/>
+          <Route path='/details' component={Details}/>
+          <Route path='/cart' component={Cart}/>
+          <Route component={Default}/>
+        </Switch>
+
+
       </React.Fragment>
-    
+    </Router>
   );
 }
 
